643a98ed40b41675655b192d3bcf951946acc7fe
flavafloyd <floydphuti911@gmail.com>
2021-10-04 18:23:58 +0200

    first commit

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -2,14 +2,16 @@ import os
 from decouple import config
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.crypto import get_random_string
+from configurations import Configuration, values
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
+class Base(Configuration):
+    SECRET_KEY = values.SecretValue()
 
-
-chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-SECRET_KEY = get_random_string(50, chars)
+# chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+# SECRET_KEY = get_random_string(50, chars)
 
 
 
