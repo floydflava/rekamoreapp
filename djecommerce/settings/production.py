9b58d9ad7237e0d65a12c78343cb423b853df4f6
flavafloyd <floydphuti911@gmail.com>
2021-10-10 00:05:06 +0200

    first commit

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 from decouple import config
 
 DEBUG = config('DEBUG')
-ALLOWED_HOSTS = ['ip-address', 'rekamoreapp.herokuapp.com']
+ALLOWED_HOSTS = ['ip-address', 'hevitall.herokuapp.com']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
