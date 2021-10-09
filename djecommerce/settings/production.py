b0becee9b711c3c316f5ab7518a68c9977e3b25b
flavafloyd <floydphuti911@gmail.com>
2021-10-10 01:58:56 +0200

    first commit

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -21,5 +21,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
 STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
