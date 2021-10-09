c39700a4bd0d9a69310b121dc9688301549df18b
flavafloyd <floydphuti911@gmail.com>
2021-10-09 18:20:14 +0200

    first commit

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 from decouple import config
 
 DEBUG = config('DEBUG')
-ALLOWED_HOSTS = ['127.0.0.1', 'rekamoreapp.herokuapp.com']
+ALLOWED_HOSTS = ['ip-address', 'rekamoreapp.herokuapp.com']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
@@ -12,24 +12,17 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('floyddipela'),
-#         'USER': config('floydflava'),
-#         'PASSWORD': config('Floydfuckme-6'),
-#         'HOST': config('127.0.0.1'),
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbmm61hsigstip',
+        'HOST': 'ec2-3-209-65-193.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'mbtuwudtrrrcuv',
+        'PASSWORD': 'cabee50906b0678a588bb6361f47ffe403548893f30d3e119d47bafb2bc016ac'
     }
 }
+
 django_heroku.settings(locals())
 STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
