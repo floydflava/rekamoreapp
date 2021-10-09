a2dfa22c9a588178554e7586acf7edf6659e6b2e
flavafloyd <floydphuti911@gmail.com>
2021-10-09 23:11:07 +0200

    first commit

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -3,7 +3,7 @@ from .base import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1','rekamoreapp.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','rekamore.herokuapp.com']
 
 
 
@@ -40,8 +40,6 @@ ALLOWED_HOSTS = ['127.0.0.1','rekamoreapp.herokuapp.com']
 #     'SHOW_TOOLBAR_CALLBACK': show_toolbar
 # }
 
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -52,22 +50,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('dbmm61hsigstip'),
-#         'USER': config('mbtuwudtrrrcuv'),
-#         'PASSWORD': config('cabee50906b0678a588bb6361f47ffe403548893f30d3e119d47bafb2bc016ac'),
-#         'HOST': config('ec2-3-209-65-193.compute-1.amazonaws.com'),
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'floyddipela',
+#         'USER': 'ibmvpyfmnzmqmn',
+#         'PASSWORD': '84b3fa85137b21f30d89023a0656b30752cc9d894d2b45574a5cf76e55e1e4ab',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
-# DATABASES['default'] =  dj_database_url.config(default='postgres://mbtuwudtrrrcuv:cabee50906b0678a588bb6361f47ffe403548893f30d3e119d47bafb2bc016ac@ec2-3-209-65-193.compute-1.amazonaws.com:5432/dbmm61hsigstip')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+django_heroku.settings(locals())
 STRIPE_PUBLIC_KEY = config('STRIPE_TEST_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_TEST_SECRET_KEY')
-django_heroku.settings(locals())
--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -15,26 +15,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('dbmm61hsigstip'),
-#         'USER': config('mbtuwudtrrrcuv'),
-#         'PASSWORD': config('cabee50906b0678a588bb6361f47ffe403548893f30d3e119d47bafb2bc016ac'),
-#         'HOST': config('ec2-3-209-65-193.compute-1.amazonaws.com'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('floyddipela'),
+#         'USER': config('floydflava'),
+#         'PASSWORD': config('Floydfuckme-6'),
+#         'HOST': config('127.0.0.1'),
 #         'PORT': '5432'
 #     }
 # }
-# DATABASES['default'] =  dj_database_url.config(default='postgres://mbtuwudtrrrcuv:cabee50906b0678a588bb6361f47ffe403548893f30d3e119d47bafb2bc016ac@ec2-3-209-65-193.compute-1.amazonaws.com:5432/dbmm61hsigstip')
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dbmm61hsigstip',
-#         'HOST': 'ec2-3-209-65-193.compute-1.amazonaws.com',
-#         'PORT': 5432,
-#         'USER': 'mbtuwudtrrrcuv',
-#         'PASSWORD': 'cabee50906b0678a588bb6361f47ffe403548893f30d3e119d47bafb2bc016ac'
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -42,6 +31,5 @@ DATABASES = {
     }
 }
 
-django_heroku.settings(locals())
 STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
