573e3d6d40a9cb23aa01e1cd33dc1e38f7fac2e4
flavafloyd <floydphuti911@gmail.com>
2021-10-10 20:07:14 +0200

    first commit

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -21,10 +21,17 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd3db6vmt3idg58',
+        'USER': 'vuslunkabhcyvg',
+        'PASSWORD': 'e8ddc08d8e208678c1b87e887d85e1caeb5843542ce96dbb8f35a1c71414743c',
+        'HOST': 'ec2-54-208-96-16.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
 
 STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
