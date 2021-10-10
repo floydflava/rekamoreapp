63a5851948a95b88b5b5c9a1540074ac8412dd8e
flavafloyd <floydphuti911@gmail.com>
2021-10-10 20:09:46 +0200

    first commit

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -14,12 +14,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
